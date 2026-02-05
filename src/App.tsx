732b544ed888e391969ced79f5cdee73310e70c2
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2026-02-05 11:23:21 -0300

    feat: Project structuring and defining project themes.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./global.css";
+
+export function App() {
+  return (
+    <>
+      <div className="title-lg font-bold">Ola app</div>
+      <div className="title-md font-bold">Ola app</div>
+      <div className="title-sm font-bold">Ola app</div>
+      <div className="text-Md font-normal">Ola app</div>
+      <div className="text-Sm font-normal">Ola app</div>
+    </>
+  );
+}
